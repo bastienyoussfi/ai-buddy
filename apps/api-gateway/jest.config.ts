da11ee78e82f6ad72e601d8c7630a7f7f6324212
byoussfi <byoussfi@steamulo.com>
2025-02-07 17:46:45 +0100

    feat(api-gateway): fix pipe

--- apps/api-gateway/jest.config.ts
+++ apps/api-gateway/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'api-gateway',
-  preset: '../../jest.preset.js',
+  preset: '../../jest.preset.mjs',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
--- jest.preset.mjs
+++ jest.preset.mjs
@@ -16,4 +16,4 @@ export default {
     '!src/**/*.interface.ts',
     '!src/**/*.dto.ts',
   ],
-};
+}; 
